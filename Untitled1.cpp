01e34b27cc9a1d38fbece0a5280bc18764221dfa
LAPTOP-QP3FMF3K\Simran Seth 48 <simranseth48@gmail.com>
2020-07-10 19:03:26 +0530

    Second change

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,6 +5,7 @@ vector<int>searchRange( vector<int> &A, int B) {
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    //Power of dynamic and engaging mindset
 
     
     
